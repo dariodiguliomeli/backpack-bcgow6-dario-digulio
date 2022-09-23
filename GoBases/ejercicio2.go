cf960068ea74237bd008eeb34b143bf99d36aecc
ddigulio <dario.digulio@mercadolibre.com>
2022-09-23 12:44:55 -0300

    GoBases ejercicio2

---
+++ GoBases/ejercicio2.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+var presion float32
+var temperatura float32
+var humedad float32
+
+func main() {
+	presion = 0.85
+	temperatura = 13
+	temperatura = .4
+
+	fmt.Println(presion)
+	fmt.Println(temperatura)
+	fmt.Println(humedad)
+}
