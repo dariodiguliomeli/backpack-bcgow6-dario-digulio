13a828e02cebc02e997583c24eba33b7e691b447
ddigulio <dario.digulio@mercadolibre.com>
2022-09-23 12:45:07 -0300

    GoBases ejercicio3

---
+++ GoBases/ejercicio3.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nombre string
+	var apellido string
+	var edad int
+	apellido = "6"
+	var licenciaDeConducir = true
+	var estaturaDeLaPersona int
+	cantidadDeHijos := 2
+
+	fmt.Println(nombre)
+	fmt.Println(apellido)
+	fmt.Println(edad)
+	fmt.Println(licenciaDeConducir)
+	fmt.Println(estaturaDeLaPersona)
+	fmt.Println(cantidadDeHijos)
+}
