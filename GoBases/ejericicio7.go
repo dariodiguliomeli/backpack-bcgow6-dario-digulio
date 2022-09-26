e5f9fb9d4298ff841c265058c974d4765e28c57c
ddigulio <dario.digulio@mercadolibre.com>
2022-09-26 10:05:25 -0300

    GoBases ejercicio7

---
+++ GoBases/ejericicio7.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var mes = 12
+	meses := map[int]string{
+		1:  "Enero",
+		2:  "Febrero",
+		3:  "Marzo",
+		4:  "Abril",
+		5:  "Mayo",
+		6:  "Junio",
+		7:  "Julio",
+		8:  "Agosto",
+		9:  "Septiembre",
+		10: "Octubre",
+		11: "Noviembre",
+		12: "Diciembre",
+	}
+	fmt.Printf("El mes %d es %v\n", mes, meses[mes])
+}
