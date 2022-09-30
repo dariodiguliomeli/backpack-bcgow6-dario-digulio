a059719c55504a936d38e21562a45cc849b5b71e
ddigulio <dario.digulio@mercadolibre.com>
2022-09-30 09:14:46 -0300

    Refactor responsibilities

--- GoBases/Hackaton/internal/service/tickets.go
+++ GoBases/Hackaton/internal/service/tickets.go
@@ -28,23 +28,33 @@ func findTicket(id int, tickets []Ticket) (Ticket, error) {
 	return Ticket{}, nil
 }
 
+func FileService() file.File {
+	return file.File{Path: filePath}
+}
+
+func readAllTickets(fileService file.File) (tickets []Ticket) {
+	tickets, err := fileService.Read()
+	check(err)
+	return
+}
+
 func (b *bookings) Create(t Ticket) (int, error) {
-	fileService := file.File{Path: filePath}
+	fileService := FileService()
 	err := fileService.Write(t)
 	check(err)
 	return t.Id, nil
 }
 
 func (b *bookings) Read(id int) (Ticket, error) {
-	fileService := file.File{Path: filePath}
-	tickets, err := fileService.Read()
-	check(err)
+	fileService := FileService()
+	tickets := readAllTickets(fileService)
 	ticket, err := findTicket(id, tickets)
 	check(err)
 	return ticket, nil
 }
 
 func (b *bookings) Update(id int, t Ticket) (Ticket, error) {
+
 	return Ticket{}, nil
 }
 
